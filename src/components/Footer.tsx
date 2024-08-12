6226e61df812e38544877cfaa93e9021d20c42a9
ezu0 <burimofumofu@gmail.com>
2024-08-12 16:38:08 +0900

    add: フッター

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py2 px-4 border-t">
+      <small>@2024 next13-blog</small>
+    </footer>
+  );
+};
+
+export default Footer;
